acc030cf31d10908e93e26e264278cc88a44dbbe
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2023-09-19 16:00:08 +0530

    Greeting Game

---
+++ timewisher.py
@@ -0,0 +1,20 @@
+import time
+tth=int(time.strftime('%H'))
+ttm=int(time.strftime('%M'))
+
+if (tth==5 and ttm>=30) or (tth>5 and tth<12):
+    print("GOOD MORNING!.")
+    print("GOOD MORNING!.")
+    print("GOOD MORNING!.")
+elif tth>=12 and tth<17:
+    print("GOOD AFTERNOON!.")
+    print("GOOD AFTERNOON!.")
+    print("GOOD AFTERNOON!.")
+elif (tth>=17 and ttm>=30) or (tth>17 and (tth==23 and ttm>30)):
+    print("GOOD EVENING!.")
+    print("GOOD EVENING!.")
+    print("GOOD EVENING!.")
+else:
+    print("YOU SHOULD BE SLEEPING!.")
+    print("YOU SHOULD BE SLEEPING!.")
+    print("YOU SHOULD BE SLEEPING!.")
