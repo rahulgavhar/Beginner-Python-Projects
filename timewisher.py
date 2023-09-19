4c633df5659ebb98d8dbaa476e9c6033786abfdf
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2023-09-19 16:25:38 +0530

    Time Wisher

--- timewisher.py
+++ timewisher.py
@@ -1,3 +1,5 @@
+#5:30 wake up
+#23:30 sleep
 import time
 tth=int(time.strftime('%H'))
 ttm=int(time.strftime('%M'))
@@ -10,11 +12,11 @@ elif tth>=12 and tth<17:
     print("GOOD AFTERNOON!.")
     print("GOOD AFTERNOON!.")
     print("GOOD AFTERNOON!.")
-elif (tth>=17 and ttm>=30) or (tth>17 and (tth==23 and ttm>30)):
+elif (tth==17 and ttm>=30) or (tth>17 and tth<=23) or (tth==23 and ttm<30):
     print("GOOD EVENING!.")
     print("GOOD EVENING!.")
     print("GOOD EVENING!.")
 else:
-    print("YOU SHOULD BE SLEEPING!.")
-    print("YOU SHOULD BE SLEEPING!.")
-    print("YOU SHOULD BE SLEEPING!.")
+    print("YOU SHOULD BE SLEEPING!")
+    print("YOU SHOULD BE SLEEPING!")
+    print("YOU SHOULD BE SLEEPING!")
