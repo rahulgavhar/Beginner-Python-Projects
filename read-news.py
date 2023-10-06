bf67605ede6d6a6ada920fc31eec82ced78929a4
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2023-10-06 23:46:20 +0530

    Get Some Knowledge

--- read-news.py
+++ read-news.py
@@ -31,35 +31,31 @@ if choice=="1":
         print("More info:",end=" ")
         print(article["url"])
 
-
 # Error Handeling For Specific Date
-topic=input("Enter a topic: ")
-current_year = time.strftime("%Y",time.localtime())
-current_month = time.strftime("%m",time.localtime())
-current_date = time.strftime("%d",time.localtime())
-yest_int_date = int(current_date)-2
 if choice=="2":
+    topic=input("Enter a topic: ")
+    current_year = time.strftime("%Y",time.localtime())
+    current_month = time.strftime("%m",time.localtime())
+    current_date = time.strftime("%d",time.localtime())
+    yest_int_date = int(current_date)-2
     if yest_int_date<10:
         yest_int_date=f"0{yest_int_date}"
     while True:
         try:
             date=input(f"Enter any date between ({yest_int_date}-{current_month}-{current_year} till 2016) in DD-MM-YYYY format: ")
             if len(date)==10:
-                if (int(date[6:10])>2015) and (int(date[6:10])<=int(current_year)):
-                    if int(date[6:10])==int(current_year):
-                        if (int(date[3:5])>0 and int(date[3:5])<=int(current_month)):
-                            if (int(date[3:5])==int(current_month)) and (int(date[0:2])>0 and int(date[0:2])<int(current_date)):
+                if (int(date[6:10])>2015) and (int(date[6:10])<int(current_year)) and (int(date[3:5])>0 and int(date[3:5])<=12) and (int(date[0:2])>0 and int(date[0:2])<=31):
+                    break
+                elif int(date[6:10])==int(current_year):
+                        if (int(date[3:5])>0 and int(date[3:5])<int(current_month)and(int(date[0:2])>0 and int(date[0:2])<=31)):
+                            break
+                        elif int(date[3:5])==int(current_month):
+                            if (int(date[0:2])>0 and int(date[0:2])<int(current_date)-1):
                                 break
-                            elif (int(date[3:5])==int(current_month)) and (int(date[0:2])>0 and int(date[0:2])==int(current_date)):
-                                raise NameError
-                            elif (int(date[3:5])==int(current_month)) and (int(date[0:2])>0 and int(date[0:2])<int(current_date)):
-                                raise IndexError
                             else:
                                 raise IndexError
                         else:
                             raise IndexError
-                    else:
-                        raise IndexError
                 else:
                     raise IndexError
             else:
