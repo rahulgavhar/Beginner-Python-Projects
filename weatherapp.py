91cbe5474c752c69d1eaa3028ce47e71a0b9c5c6
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2023-10-15 20:36:22 +0530

    Know Weather Reports of any Location

--- weatherapp.py
+++ weatherapp.py
@@ -21,11 +21,12 @@ response=requests.get(url).json()
 print(f"\n*Current Weather Report of {city}*\n")
 def kelvin_to_celsius(t):
     return f"{round(t-273.15,2)}°C", f"{round(((t-273.15)*(9/5))+32,2)}°F"
-
+print(f"Temperature: {kelvin_to_celsius(response['main']['temp'])[0]} ({kelvin_to_celsius(response['main']['temp'])[1]})")
+print(f"Humidity: {response['main']['humidity']}%")
 print(f"Feels like: {kelvin_to_celsius(response['main']['feels_like'])[0]} ({kelvin_to_celsius(response['main']['feels_like'])[1]})")
 
 print("General Weather: ",response['weather'][0]['description'])
-print("Sunrise Time: ",dt.datetime.fromtimestamp(response['sys']['sunrise']).strftime('%H:%M'), "Sunset Time: ",dt.datetime.fromtimestamp(response['sys']['sunset']).strftime('%H:%M'))
+print("Sunrise Time: ",dt.datetime.fromtimestamp(response['sys']['sunrise']).strftime('%H:%M'),"IST", "\nSunset Time: ",dt.datetime.fromtimestamp(response['sys']['sunset']).strftime('%H:%M'),"IST")
 
 name=response['name']
 print(f"\nData has been fetched from the Location: {name}, ({getLoc.address})")
