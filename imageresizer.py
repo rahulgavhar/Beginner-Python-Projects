e0f18826ad7e8db7679acc94d1751621f97ac7cc
Rahul Gavhar <138155613+rahulgavhar@users.noreply.github.com>
2023-10-15 18:35:12 +0530

    Resize(dimensions)/ Reduce Image File Size

--- imageresizer.py
+++ imageresizer.py
@@ -1,5 +1,5 @@
-#Requirements: Python 3.6.5 or higher, OpenCV 4.5.1 or higher, pillow module
-#Folders: Image Operation > Input, Output, Dustbin, imageresizer.py
+#Requirements: Python 3.6.5 or higher, OpenCV 4.5.1 or higher, import pillow module
+#Folders: Resize Image > Input, Output, Dustbin, imageresizer.py
 
 import cv2
 import os
@@ -23,17 +23,33 @@ except Exception as e:
     print("Error: ",e)
 
 if run=="yes":
-    method=input("Enter the operation for image\n1. Change File Size\n2. Change Dimensions\nEnter 1 or 2: ")
+    while True:
+        try:
+            method=input("Enter the operation for image\n1. Change File Size\n2. Change Dimensions\nEnter 1 or 2: ")
+            if method=="1" or method=="2":
+                break
+            else:
+                raise Exception("Invalid Input")
+        except Exception as e:
+            print(e)
 
     if method=="1":
         original_size = os.path.getsize(f"Image Operation/Input/{filename}")
-        og_size_inmb=float(original_size/2**20)
+        og_size_inmb=round(float(original_size/2**20),4)
         print(f"Original Size: {og_size_inmb} MB")
-        qlt=int(input("Enter the new quality of the image (1-50)for compression, (50<)for expansion: "))
+        while True:
+            qlt=int(input("Enter the new quality of the image (1-50)for compression, (50-100)for expansion: "))
+            if qlt>=1 and qlt<=100:
+                break
+            elif qlt>50:
+                break
+            else:
+                print("Preffered quality is between 1-100")
+                continue
         image = Image.open(f"Image Operation/Input/{filename}")
         image.save(f"Image Operation/Output/{filename.split('.')[0]}_reduced.jpg", optimize=True, quality=qlt)
         compressed_size = os.path.getsize(f"Image Operation/Output/{filename.split('.')[0]}_reduced.jpg")
-        cs_size_inmb=float(compressed_size/2**20)
+        cs_size_inmb=round(float(compressed_size/2**20),4)
         print(f"\nCompressed Size: {cs_size_inmb} MB\nCompression Percentage: {round((original_size-compressed_size)/original_size*100,2)}%")
         # Removing the image from the input directory to dustbin folder
         os.makedirs("Image Operation/Dustbin", exist_ok=True)
@@ -44,36 +60,40 @@ if run=="yes":
             src = cv2.imread(f"Image Operation/Input/{filename}", cv2.IMREAD_UNCHANGED)
             print(f'Original Dimensions (w*h): {src.shape[1]}x{src.shape[0]}')
             print("1. Dimensions to which you want to scale your image (width, height)\n2. The \'%\' to which you want to scale your image (dimensions)")
-            dimorpercent=input("Enter 1 or 2: ")
-            if dimorpercent=="1":
-                while True:
-                    try:
-                        new_width=input("Enter the dimensions(width): ")
-                        new_height=input("Enter the dimensions(height): ")
-                        new_width = int(new_width)
-                        new_height = int(new_height)
-                        times_w=new_width/src.shape[1]
-                        times_h=new_width/src.shape[0]
-                        if times_w>100 or times_h>100:
-                            raise Exception("The dimensions you entered are x100 greater than the original dimensions")
-                        break
-                    except Exception as e:
-                        print(e)
-                        continue
-                    
-            elif dimorpercent=="2":
-                scale_percent = int(input("Enter the \'%\' to which you want to scale your image (dimensions): "))
-                while True:
-                    try:
-                        if scale_percent<10000:
-                            new_width = int(src.shape[1] * scale_percent / 100)
-                            new_height = int(src.shape[0] * scale_percent / 100)
-                        else:
-                            raise Exception("The image cannot be scaled to more than 100 times or 10000% ")
-                        break
-                    except Exception as e:
-                        print(e)
-                        continue
+            while True:
+                dimorpercent=input("Enter 1 or 2: ")
+                if dimorpercent=="1":
+                    while True:
+                        try:
+                            new_width=input("Enter the dimensions(width): ")
+                            new_height=input("Enter the dimensions(height): ")
+                            new_width = int(new_width)
+                            new_height = int(new_height)
+                            times_w=new_width/src.shape[1]
+                            times_h=new_width/src.shape[0]
+                            if times_w>10 or times_h>10:
+                                raise Exception("The dimensions you entered are x10 greater than the original dimensions")
+                            break
+                        except Exception as e:
+                            print(e)
+                    break
+                elif dimorpercent=="2":
+                    scale_percent = int(input("Enter the \'%\' to which you want to scale your image (dimensions): "))
+                    while True:
+                        try:
+                            if scale_percent<1000:
+                                new_width = int(src.shape[1] * scale_percent / 100)
+                                new_height = int(src.shape[0] * scale_percent / 100)
+                                break
+                            else:
+                                raise Exception("The image cannot be scaled to more than 10 times or 1000% ")
+                        except Exception as e:
+                            print(e)
+                            continue
+                    break
+                else:
+                    print("Invalid Input")
+                    continue
             dim = (new_width, new_height)
             output = cv2.resize(src, dim)
             cv2.imwrite(f"Image Operation/Output/{filename.split('.')[0]}_resized.jpg", output)
